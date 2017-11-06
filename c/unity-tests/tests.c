20d73ba45c40eb8da93f5b8fdbcf3ced51a03ce0
Santiago Munoz <smr.rambar@gmail.com>
2017-11-06 11:18:48 +0100

    Changed example to use test fixtures

--- c/unity-tests/tests.c
+++ c/unity-tests/tests.c
@@ -1,13 +1,51 @@
 #include "Unity/src/unity.h"
 #include "functions.h"
+#include "Unity/extras/fixture/src/unity_fixture.h"
 
-void test_addition_works(void){
-    TEST_ASSERT_EQUAL_HEX16(6,add_three_bytes(1,2,3));
-    TEST_ASSERT_EQUAL_HEX16(65,add_three_bytes(0, 50, 15));
+TEST_GROUP(sample);
+
+uint16_t a, b, c;
+
+TEST_SETUP(sample)
+{
+    a=0;
+    b=0;
+    c=0;
 }
 
-int main(){
+TEST_TEAR_DOWN(sample)
+{
+}
+
+TEST(sample, no_params)
+{
+    TEST_ASSERT_EQUAL_HEX16(0,add_three_bytes(a,b,c));
+}
+
+TEST(sample, one_param)
+{
+    a=5;
+    TEST_ASSERT_EQUAL_HEX16(5,add_three_bytes(a,b,c));
+}
+
+TEST(sample, full_addition)
+{
+    a=5;
+    b=1;
+    c=4;
+    TEST_ASSERT_EQUAL_HEX16(10,add_three_bytes(a,b,c));
+}
+
+TEST_GROUP_RUNNER(sample)
+{
+    RUN_TEST_CASE(sample, no_params);
+    RUN_TEST_CASE(sample, one_param);
+    RUN_TEST_CASE(sample, full_addition);
+
+}
+int main()
+{
     UNITY_BEGIN();
-    RUN_TEST(test_addition_works);
+    RUN_TEST_GROUP(sample);
     return UNITY_END();
 }
